5e535dd0ee8074153c7ea596baa64f0409723ae6
synzen <michael994664@gmail.com>
2020-11-08 17:11:39 -0500

    Pass options object to connectToDatabase

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import connectToDatabase from './utils/connectToDatabase';
 
 (async function start() {
   try {
-    await connectToDatabase(appConfig.databaseURI);
+    await connectToDatabase(appConfig.databaseURI, {
+      // Indexes are ensured within the delivery service app
+      ensureIndexes: false
+    });
     console.log('Conencted to mongo');
   } catch (err) {
     console.error(err);
--- src/utils/connectToDatabase.ts
+++ src/utils/connectToDatabase.ts
@@ -1,13 +1,12 @@
-import { MikroORM } from '@mikro-orm/core';
+import { Configuration, Connection, IDatabaseDriver, MikroORM, Options } from '@mikro-orm/core';
 import DeliveryRecord from '../entities/DeliveryRecord';
 
-const connectToDatabase = async (uri: string) => {
+const connectToDatabase = async (uri: string, options?: Configuration<IDatabaseDriver<Connection>>|Options<IDatabaseDriver<Connection>>) => {
   const orm = await MikroORM.init({
     entities: [DeliveryRecord],
     type: 'mongo',
     clientUrl: uri,
-    // This app should only read from records
-    ensureIndexes: false,
+    ...options
   });
   return orm;
   // console.log(orm.em); // access EntityManager via `em` property
